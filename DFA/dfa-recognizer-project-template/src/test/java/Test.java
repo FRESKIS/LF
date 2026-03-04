dec51bd9886b24b43d655b670e53b514bbb95c51
Sergi Molina <sergistile@gmail.com>
2026-03-04 18:21:58 +0100

    dfa-4 arreglat :(

--- DFA/dfa-recognizer-project-template/src/test/java/Test.java
+++ DFA/dfa-recognizer-project-template/src/test/java/Test.java
@@ -18,7 +18,7 @@ public class Test {
 
     @ParameterizedTest
     @CsvSource({
-            "testcases/dfa-member2.txt, testcases/input-member2.txt, testcases/output-member2.txt",
+            "testcases/dfa-member4.txt, testcases/input-member4.txt, testcases/output-member4.txt",
     })   
     public void testOnDefaultInput(String dfaSpecFilePath, String inputFilePath, String outputFilePath) throws IOException, URISyntaxException {
         //Loading files
