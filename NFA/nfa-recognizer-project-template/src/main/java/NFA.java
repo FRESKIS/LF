ad221d2acc42f0eee4a7f73bcfef60541c6f37f1
Sergi Molina <sergistile@gmail.com>
2026-03-05 09:16:11 +0100

    Initial methods of NFA and DFA impl

--- NFA/nfa-recognizer-project-template/src/main/java/NFA.java
+++ NFA/nfa-recognizer-project-template/src/main/java/NFA.java
@@ -1,38 +1,45 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class NFA {
+
+    String[] states;
+    List<trans> transitions = new ArrayList<>();
+    String inicial;
+    List<String> finals = new ArrayList<>();
     
     public NFA(String[] states) {
-        //your code goes here
+        this.states = states;
     }
 
     public void setInitialState(String state) {
-        // your code goes here
+        this.inicial = state;
     }
 
     public String getInitialState() {
         // your code goes here
-        return null;
+        return this.inicial;
     }
 
     public void addFinalState(String state) {
-        // your code goes here
+        this.finals.add(state);
     }
 
     // Method to add a transition to the table
     public void addTransition(String state, Character input, String nextState) {
-        // your code goes here
+        this.transitions.add(new trans(state, Label.createNonEmptyLabel(input), nextState));
     }
 
     public void addEpsilonTransition(String state, String nextState) {
-        // your code goes here
+        this.transitions.add(new trans(state, Label.createEmptyLabel(), nextState));
     }
 
     // Outputs true if the NFA accept the input string
     //The strategy we follow is to convert the NFA into a DFA
     public boolean accept(String input) {
-        // your code goes here
-        return false;
+        DFA dfa = NFAtoDFA();
+        return dfa.accept(input);
     }
 
     public DFA NFAtoDFA(){
@@ -43,6 +50,18 @@ public class NFA {
     public Set<Label> getAllLabels(){
         // your code goes here
         return null;
+    }
+
+    public class trans {
+        public String state;
+        public Label input;
+        public String nextState;
+
+        public trans(String state, Label input, String nextState) {
+            this.state = state;
+            this.input = input;
+            this.nextState = nextState;
+        }
     }
                     
        
