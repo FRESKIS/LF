287e993ed990469b405a81a5d2e23dcb74720b6f
gambacanastando <brpuig05@gmail.com>
2026-03-14 13:25:29 +0100

    re-member3

--- RE/regular-expression-project-template/src/test/java/Test.java
+++ RE/regular-expression-project-template/src/test/java/Test.java
@@ -20,10 +20,10 @@ public class Test {
     @CsvSource({
             //"testcases/re1.txt, testcases/input1.txt, testcases/output1.txt",
             //"testcases/re2.txt, testcases/input2.txt, testcases/output2.txt",
-            "testcases/re-member1.txt, testcases/re-member1-input.txt, testcases/re-member1-output.txt",
-            "testcases/re-member2.txt, testcases/re-member2-input.txt, testcases/re-member2-output.txt",
-            //"testcases/re-member3.txt, testcases/re-member3-input.txt, testcases/re-member3-output.txt",
-            "testcases/re-member4.txt, testcases/re-member4-input.txt, testcases/re-member4-output.txt",
+            //"testcases/re-member1.txt, testcases/re-member1-input.txt, testcases/re-member1-output.txt",
+            //"testcases/re-member2.txt, testcases/re-member2-input.txt, testcases/re-member2-output.txt",
+            "testcases/re-member3.txt, testcases/re-member3-input.txt, testcases/re-member3-output.txt",
+            //"testcases/re-member4.txt, testcases/re-member4-input.txt, testcases/re-member4-output.txt",
     })   
     public void testOnDefaultInput(String regexSpecFilePath, String inputFilePath, String outputFilePath) throws IOException, URISyntaxException {
         //Loading files
